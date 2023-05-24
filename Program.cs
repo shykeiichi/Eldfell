64e3b7c35b5ca8bae675d5b1aaca1e7487e04f25
shykeiichi <imdark200@gmail.com>
2023-05-24 23:18:36 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System;
+
+namespace Eldfell;
+
+class Program
+{
+    [Get("/")]
+    Response Index()
+    {
+        return new RenderTemplate("");
+    }
+
+    [Get("/User/:name")]
+    Response User(string name)
+    {
+        return new Response(name);
+    }
+
+    public static void Main(string[] args)
+    {
+    
+    }
+}
